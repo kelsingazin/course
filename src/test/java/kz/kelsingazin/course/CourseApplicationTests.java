73a47bb0e624837a8a35920062ceaf129e5c7c02
kelsingazin <kelsingaziny@gmail.com>
2023-01-28 19:53:03 +0600

    init commit

---
+++ src/test/java/kz/kelsingazin/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kelsingazin.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
